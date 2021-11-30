cdba8e37c300a70fa6321f12fcd8c74207d9c745
liwenzhi <lwz7512@gmail.com>
2021-11-29 21:36:21 -0400

    add project icon

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,10 +2,24 @@ import { Link, } from 'react-router-dom';
 
 const ProjectCard = ({ project }) => (
   <div className="p-col-12 w-full sm:w-6 lg:w-4" >
-      <div className="feature-card">
-          <img alt="components" src={project.image} />
+      <div className="feature-card zoom">
+          <img className="" alt="components" src={project.image} />
+          <div className="top-row">
+            <span className="px-1">
+              <img src="assets/icon/favourite.png" className="star-icon" alt="star" />
+            </span>
+            <span className="px-1">
+              <img src="assets/icon/favourite.png" className="star-icon" alt="star" />
+            </span>
+            <span className="px-1">
+              <img src="assets/icon/favourite.png" className="star-icon" alt="star" />
+            </span>
+            <img src="assets/icon/medal.png" className="complete-badge-icon" alt="badge" />
+          </div>
+          <div className="footer-row">
+            <h3>{project.title}</h3>
+          </div>
           <div className="feature-card-detail">
-              <h2 className="feature-name text-2xl">{project.title}</h2>
               <p className="text-xl h-8rem">{project.description}</p>
           </div>
           <div className="footer p-5">
